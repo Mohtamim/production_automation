aa8d93dd2a5c4aa6f51f41c1c5defd972596fa9c
Mohtamim <mohtamimnayeem@gmail.com>
2022-08-29 16:51:24 +0600

    route created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,17 @@
 <?php
 
+use App\Http\Controllers\AssainedOrderController;
+use App\Http\Controllers\BankBenificiaryController;
+use App\Http\Controllers\BuyersController;
+use App\Http\Controllers\CatController;
+use App\Http\Controllers\CountryController;
+use App\Http\Controllers\MainOrderController;
+use App\Http\Controllers\ManagerlistController;
+use App\Http\Controllers\PackagingController;
+use App\Http\Controllers\ShiperAndExporterController;
+use App\Http\Controllers\SubCatController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\WarehouseController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +28,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.layout');
 });
+
+Route::resource('assaign_order', AssainedOrderController::class);
+Route::resource('users', UsersController::class);
+Route::resource('country', CountryController::class);
+Route::resource('bank_benificiary', BankBenificiaryController::class);
+Route::resource('buyers', BuyersController::class);
+Route::resource('category', CatController::class);
+Route::resource('main_order', MainOrderController::class);
+Route::resource('managers', ManagerlistController::class);
+Route::resource('packaging', PackagingController::class);
+Route::resource('port', PackagingController::class);
+Route::resource('shiperment_exporter', ShiperAndExporterController::class);
+Route::resource('sub_category', SubCatController::class);
+Route::resource('warehouses', WarehouseController::class);
