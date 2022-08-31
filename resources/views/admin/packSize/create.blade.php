d2c1b64b13b75c408e3f83b95b0da01ebe20f38d
Mohtamim <mohtamimnayeem@gmail.com>
2022-08-31 18:00:55 +0600

    pack size completed

--- resources/views/admin/packSize/create.blade.php
+++ resources/views/admin/packSize/create.blade.php
@@ -20,7 +20,7 @@
                 <form class="" action="{{ url('admin/pack_size') }}" method="POST">
                     {!! csrf_field() !!}
                     <div class="input-group mb-3">
-                        <span class="input-group-text bg-light text-black font-weight-bold" id="title">Main Order
+                        <span class="input-group-text bg-light text-black font-weight-bold" id="title">Pack Name
                             Id:</span>
                         <input type="text"
                             class="form-control @error('title')
@@ -31,12 +31,8 @@
                             <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
                         @enderror
                     </div>
-
-
-
-
                     <div class="input-group mb-3">
-                        <span class="input-group-text bg-light text-black font-weight-bold" id="length">Ware House
+                        <span class="input-group-text bg-light text-black font-weight-bold" id="length">Length
                             Id:</span>
                         <input type="number"
                             class="form-control @error('length')
@@ -53,81 +49,7 @@
                     <div class="input-group mb-3">
                         <span class="input-group-text bg-light text-black font-weight-bold" id="phone">width:</span>
                         <input type="number"
-                            class="form-control @error('quantity')
-                     is-invalid
-                    @enderror"
-                            name="width" placeholder="Enter  width" aria-label="Enter  width "
-                            aria-describedby="basic-addon2">
-                        @error('width')
-                            <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
-                        @enderror
-                    </div>
-
-
-                    <input type="submit" value="save" class="btn btn-success">
-                    <button class="btn btn-secondary">Cancel</button>
-                </form>
-
-            </div>
-        </div>
-    </div>
-@endsection
-@extends('admin.layout')
-@section('ware-title')
-    WareHose
-@endsection
-@section('admin_content')
-    <div class="col-lg-12 col-12  layout-spacing">
-        <div class="statbox widget box box-shadow">
-            <div class="widget-header">
-                <div class="row">
-                    <div class="col-xl-12 col-md-12 col-sm-12 col-12 d-flex ">
-                        <h4 class="text-center"> Assaign Order</h4>
-                        <a href="{{ url('admin/pack_size') }}" class="btn btn-info text-center justify-content-start my-4">
-                            Assaign Order</a>
-                    </div>
-
-                </div>
-
-            </div>
-            <div class="widget-content widget-content-area">
-                <form class="" action="{{ url('admin/pack_size') }}" method="POST">
-                    {!! csrf_field() !!}
-                    <div class="input-group mb-3">
-                        <span class="input-group-text bg-light text-black font-weight-bold" id="title">Main Order
-                            Id:</span>
-                        <input type="text"
-                            class="form-control @error('title')
-                     is-invalid
-                    @enderror"
-                            name="title" placeholder="Enter MainOrder Id">
-                        @error('title')
-                            <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
-                        @enderror
-                    </div>
-
-
-
-
-                    <div class="input-group mb-3">
-                        <span class="input-group-text bg-light text-black font-weight-bold" id="length">Ware House
-                            Id:</span>
-                        <input type="number"
-                            class="form-control @error('length')
-                    is-invalid
-                    @enderror"
-                            name="length" placeholder="Enter length" aria-label="Enter your Assign Order Id"
-                            aria-describedby="basic-addon2">
-                        @error('length')
-                            <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
-                        @enderror
-
-                    </div>
-
-                    <div class="input-group mb-3">
-                        <span class="input-group-text bg-light text-black font-weight-bold" id="phone">width:</span>
-                        <input type="number"
-                            class="form-control @error('quantity')
+                            class="form-control @error('width')
                      is-invalid
                     @enderror"
                             name="width" placeholder="Enter  width" aria-label="Enter  width "
@@ -137,14 +59,14 @@
                         @enderror
                     </div>
                     <div class="input-group mb-3">
-                        <span class="input-group-text bg-light text-black font-weight-bold" id="phone">width:</span>
+                        <span class="input-group-text bg-light text-black font-weight-bold" id="phone">Height:</span>
                         <input type="number"
-                            class="form-control @error('quantity')
+                            class="form-control @error('height')
                      is-invalid
                     @enderror"
-                            name="width" placeholder="Enter  width" aria-label="Enter  width "
+                            name="height" placeholder="Enter  height" aria-label="Enter  height "
                             aria-describedby="basic-addon2">
-                        @error('width')
+                        @error('height')
                             <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
                         @enderror
                     </div>
--- resources/views/admin/packSize/edit.blade.php
+++ resources/views/admin/packSize/edit.blade.php
@@ -1,72 +1,84 @@
 @extends('admin.layout')
 @section('ware-title')
-WareHose
+    WareHose
 @endsection
 @section('admin_content')
-<div class="col-lg-12 col-12  layout-spacing">
-    <div class="statbox widget box box-shadow">
-        <div class="widget-header">
-            <div class="row">
-                <div class="col-xl-12 col-md-12 col-sm-12 col-12">
-                    <h4 class="text-center">Edit Assaign Order</h4>
+    <div class="col-lg-12 col-12  layout-spacing">
+        <div class="statbox widget box box-shadow">
+            <div class="widget-header">
+                <div class="row">
+                    <div class="col-xl-12 col-md-12 col-sm-12 col-12 d-flex ">
+                        <h4 class="text-center"> Assaign Order</h4>
+                        <a href="{{ url('admin/pack_size') }}" class="btn btn-info text-center justify-content-start my-4">
+                            Assaign Order</a>
+                    </div>
+
                 </div>
+
+            </div>
+            <div class="widget-content widget-content-area">
+                <form class="" action="{{ url('admin/pack_size/'.$pack->id) }}" method="POST">
+                    {!! csrf_field() !!}
+                    @method('PATCH')
+                    <div class="input-group mb-3">
+                        <span class="input-group-text bg-light text-black font-weight-bold" value="{{ $pack->title }}" id="title">Pack Name
+                            Id:</span>
+                        <input type="text"
+                            class="form-control @error('title')
+                     is-invalid
+                    @enderror"
+                            name="title" placeholder="Enter a Pack Name">
+                        @error('title')
+                            <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                        @enderror
+                    </div>
+                    <div class="input-group mb-3">
+                        <span class="input-group-text bg-light text-black font-weight-bold" value="{{ $pack->length }}" id="length">Length
+                            Id:</span>
+                        <input type="number"
+                            class="form-control @error('length')
+                    is-invalid
+                    @enderror"
+                            name="length" placeholder="Enter length" aria-label="ex:1.5x2.6"
+                            aria-describedby="basic-addon2">
+                        @error('length')
+                            <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                        @enderror
+
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <span class="input-group-text bg-light text-black font-weight-bold" value="{{ $pack->width }}" id="phone">width:</span>
+                        <input type="number"
+                            class="form-control @error('width')
+                     is-invalid
+                    @enderror"
+                            name="width" placeholder="ex:1.5x2.5" aria-label="Enter  width "
+                            aria-describedby="basic-addon2">
+                        @error('width')
+                            <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                        @enderror
+                    </div>
+
+                    <div class="input-group mb-3">
+                        <span class="input-group-text bg-light text-black font-weight-bold" value="{{ $pack->height }}" id="phone">Height:</span>
+                        <input type="number"
+                            class="form-control @error('height')
+                     is-invalid
+                    @enderror"
+                            name="height" placeholder="ex:1.5x2.5" aria-label="Enter  height "
+                            aria-describedby="basic-addon2">
+                        @error('height')
+                            <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+                        @enderror
+                    </div>
+
+
+                    <input type="submit" value="save" class="btn btn-success">
+                    <button class="btn btn-secondary">Cancel</button>
+                </form>
+
             </div>
         </div>
-        <div class="widget-content widget-content-area">
-            <form class="" action="{{ url('admin/assaign_order/'.$assain->id) }}" method="POST">
-                {!! csrf_field() !!}
-                @method('PUT')
-                <div class="input-group mb-3">
-                    <span class="input-group-text bg-light text-black font-weight-bold" id="mainOrderId">Main Order Id:</span>
-                    <input type="number" value="{{$assain->mainOrderId}}" class="form-control @error('mainOrderId')
-                     is-invalid
-                    @enderror" name="mainOrderId" placeholder="Enter your Main Order Id">
-                    @error('mainOrderId')
-                     <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
-                    @enderror
-                </div>
-                <div class="input-group mb-3">
-                    <span class="input-group-text bg-light text-black font-weight-bold" id="warehouseId">Ware House Id:</span>
-                    <input type="number" value="{{$assain->warehouseId}}" class="form-control @error('warehouseId')
-                    is-invalid
-
-                    @enderror" name="warehouseId" placeholder="Enter your warehouseId" aria-label="Enter your Assign Order Id" aria-describedby="basic-addon2">
-                    @error('warehouseId')
-                    <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
-                    @enderror
-
-                </div>
-
-                <div class="input-group mb-3">
-                    <span class="input-group-text bg-light text-black font-weight-bold" id="phone">Quantity:</span>
-                    <input type="number" value="{{$assain->quantity}}" class="form-control @error('quantity')
-                     is-invalid
-                    @enderror" name="quantity" placeholder="Enter your quantity" aria-label="Enter your quantity " aria-describedby="basic-addon2">
-                    @error('quantity')
-                    <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
-
-                    @enderror
-                </div>
-                <div class="input-group mb-3" style="minWidth:678px">
-
-                    <select id="status" class="form-select" name="status">
-                        <option value="" >Select Status</option>
-                        @if ($assain->status==1)
-                        <option value="1" selected>Active</option>
-                        @endif
-                        @if ($assain->status==0)
-                        <option value="0" selected>Deactive</option>
-                        @endif
-                        <option value="1">Active</option>
-                        <option value="0">Deactive</option>
-                    </select>
-                </div>
-
-                <input type="submit" value="save" class="btn btn-success">
-                <button class="btn btn-secondary">Cancel</button>
-              </form>
-
-        </div>
     </div>
-</div>
 @endsection
