ee938e15b9f793638b5fb390be3a444ffdd4d874
Mohtamim <mohtamimnayeem@gmail.com>
2022-09-08 11:27:04 +0600

    authenticated

--- app/Http/Controllers/customLoginController.php
+++ app/Http/Controllers/customLoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\authUser;
+use App\Models\User;
 use Session;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -13,13 +14,20 @@ class customLoginController extends Controller
     }
     public  function login(Request $request){
         $input=$request->all();
+        $userName=$request->userName;
         $this->validate($request,[
             'userName'=>'required',
             'password'=>'required',
 
         ]);
         if(Auth::guard('user')->attempt(['userName'=>$input['userName'],'password'=>$input['password']])){
-            return redirect('admin/dashboard');
+            $userType = User::where('userName',$userName)->value('userType');
+            if($userType==1){
+
+                return redirect('admin/dashboard');
+            }else{
+                return redirect('manager/dashboard');
+            }
         }else{
             return redirect()->back()->with('error','Invaalid Username or password');
         }
