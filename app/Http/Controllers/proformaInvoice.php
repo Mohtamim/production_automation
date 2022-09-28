ee2d69e09fd7ab54ecbedb7899fa20eb0e9881d2
Mohtamim <mohtamimnayeem@gmail.com>
2022-09-28 10:13:16 +0600

    suplieer

--- app/Http/Controllers/proformaInvoice.php
+++ app/Http/Controllers/proformaInvoice.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\mainOrder;
 use Illuminate\Http\Request;
 
 class proformaInvoice extends Controller
@@ -9,5 +10,10 @@ class proformaInvoice extends Controller
     public function index()
     {
         return view('admin.invoice.proformaInvoice');
+    }
+    public function pidata($id)
+    {
+        $order = mainOrder::where('id', $id)->with(['buyers', 'products'])->select()->get();
+        return response()->json($order, 200);
     }
 }
