052587f85098050bcde47ef71eec673ce4b3b5b5
Mohtamim <mohtamimnayeem@gmail.com>
2022-09-28 14:33:56 +0600

    invoice

---
+++ app/Http/Controllers/manager/SupplierAssaignOrderController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\manager;
+
+use App\Http\Controllers\Controller;
+
+use App\Models\supplierAssaignOrder;
+use Illuminate\Http\Request;
+
+class SupplierAssaignOrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\supplierAssaignOrder  $supplierAssaignOrder
+     * @return \Illuminate\Http\Response
+     */
+    public function show(supplierAssaignOrder $supplierAssaignOrder)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\supplierAssaignOrder  $supplierAssaignOrder
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(supplierAssaignOrder $supplierAssaignOrder)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\supplierAssaignOrder  $supplierAssaignOrder
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, supplierAssaignOrder $supplierAssaignOrder)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\supplierAssaignOrder  $supplierAssaignOrder
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(supplierAssaignOrder $supplierAssaignOrder)
+    {
+        //
+    }
+}
